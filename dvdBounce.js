60b0e36919b8065420359ebaa484c0d8da87f9bd
unknown <mrazzakov@gmail.com>
2019-03-08 21:49:44 -0600

    color changes on direction change

--- dvdBounce.js
+++ dvdBounce.js
@@ -1,44 +1,63 @@
  
  
- function myMove() {
-    var logo = document.getElementById("logo");
-    var width = window.innerWidth - logo.width;
-    var height = window.innerHeight - logo.height;
+ function moveLogo() {
+    var logo = document.getElementById('logo');    
+
+    var width = window.innerWidth - logo.clientWidth;
+    var height = window.innerHeight - logo.clientHeight;
     var xPos = Math.floor(Math.random() * width);
     var yPos = Math.floor(Math.random() * height);
-    var xSpeed = 1;
-    var ySpeed = 1;
+    var xSpeed = Math.floor(Math.random() * 2) ? 1 : -1;
+    var ySpeed = Math.floor(Math.random() * 2) ? 1 : -1;
+    const BUFFER_WIDTH = 10;
+    const BUFFER_HEIGHT = 10;
 
     
-    setInterval(frame, 5);
+    setInterval(frame, 10);
     
     function frame() {
-        ratio = logo.width/logo.naturalWidth;
+        ratio = logo.clientWidth/250;
         xSpeed = Math.sign(xSpeed) * ratio;
         ySpeed = Math.sign(ySpeed) * ratio;
 
-        console.log(xSpeed);
-        console.log(ySpeed);
-        width = window.innerWidth - logo.width;
-        height = window.innerHeight - logo.height;
+        width = window.innerWidth - logo.clientWidth + BUFFER_WIDTH;
+        height = window.innerHeight - logo.clientHeight + BUFFER_HEIGHT;
         
-        if(xPos > width || xPos < 0) {
+        if(xPos > width || xPos < -5) {
             xSpeed = -xSpeed;
             xPos = Math.min(xPos + xSpeed, width + xSpeed);
+            changeColor();
         }
         else {
             xPos += xSpeed;
             logo.style.left = xPos + "px";
         }
 
-        if(yPos > height || yPos < 0) {
+        if(yPos > height || yPos < -5) {
             ySpeed = -ySpeed;
             yPos = Math.min(yPos + ySpeed, height + ySpeed);
+            changeColor();
         }
         else {
             yPos += ySpeed;
             logo.style.top = yPos + "px";
         }
+    }
+
+
+
+    function changeColor() {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+       
+        //to keep colors vivid, 1 of the 3 RGB need to be 0 at all times;
+        let select = Math.floor(Math.random() * 3);
+        select == 0 ? r=0 : select == 1 ? g=0 : b=0;
+        logo.style.fill = 'rgb(' + r + ',' + g + "," + b + ')';
+
+
+        
     }
  }
 
